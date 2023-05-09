6666c86014953aeb6fb66dd15d67979d00fa18f0
Gabriel Mascaretti <gabrielmascaretti@gmail.com>
2023-05-08 21:30:56 -0300

    cambios

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000; 
+
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Servidor corriendo el puerto 3000');
 });
 
@@ -14,10 +16,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
-app.get('/register.html', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
 
-app.get('/login.html', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
